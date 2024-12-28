21e894cb0d20e5bde1c46776b27c62bbb514c493
leiassyun <sy3544@nyu.edu>
2024-12-28 00:47:24 -0500

    Add Sound

---
+++ IntervalTimer/Managers/SoundManager.swift
@@ -0,0 +1,25 @@
+import AVFoundation
+
+class SoundManager {
+    static let shared = SoundManager()
+    private var audioPlayer: AVAudioPlayer?
+
+    private init() {} 
+
+    func playSound(named fileName: String, withExtension fileExtension: String = "mp3") {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else {
+            print("Sound file not found: \(fileName).\(fileExtension)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+
+    func stopSound() {
+        audioPlayer?.stop()
+    }
+}
